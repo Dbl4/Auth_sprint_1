3e18d46694239182eec964d2b341c7cb91bfd171
vladislav erofeev <vladislav.erofeev@zyfra.com>
2023-03-15 19:57:17 +0300

    initial

--- auth/app.py
+++ auth/app.py
@@ -1,12 +1,11 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/hello-world')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/login')
+def login():
+    return render_template("login.html", title="Авторизация")
 
 
 if __name__ == '__main__':
