ec8fb8fa8f698ba35b22b594a208f7867193a0a0
vladislav erofeev <vladislav.erofeev@zyfra.com>
2023-03-18 23:37:00 +0300

    test create_db

--- auth/settings.py
+++ auth/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings, PostgresDsn, Field
 
 
 class Settings(BaseSettings):
-    auth_postgres_url: PostgresDsn = Field("postgresql://app:123qwe@localhost:54321/auth",
+    auth_postgres_url: PostgresDsn = Field("postgresql://app:123qwe@localhost:5432/auth",
                                            env="AUTH_POSTGRES_URL")
 
 
