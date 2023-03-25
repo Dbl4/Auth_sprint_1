907973bee53bcde94e5e374b611b75d61c05f4c2
rtmvnv <rtmvnv@yandex.ru>
2023-03-25 11:33:30 +0300

    Compile postgres url using urllib

--- auth/app.py
+++ auth/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from db import db, init_db
+from urllib.parse import urlunsplit
 
 from models import User, Role
 from flask import Flask
@@ -7,17 +8,19 @@ from flask import Flask
 from settings import settings
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "postgresql://"
-    + settings.auth_postgres_user
-    + ":"
-    + settings.auth_postgres_password
-    + "@"
-    + settings.auth_postgres_host
-    + ":"
-    + str(settings.auth_postgres_port)
-    + "/"
-    + settings.auth_postgres_db
+app.config["SQLALCHEMY_DATABASE_URI"] = urlunsplit(
+    (
+        "postgresql",
+        (
+            f"{settings.auth_postgres_user}"
+            f":{settings.auth_postgres_password}"
+            f"@{settings.auth_postgres_host}"
+            f":{settings.auth_postgres_port}"
+        ),
+        settings.auth_postgres_db,
+        "",
+        "",
+    ),
 )
 
 app.app_context().push()
@@ -30,16 +33,16 @@ def hello_world():
 
 
 def main():
-    role = Role(name='actor')
+    role = Role(name="actor")
     db.session.add(role)
 
-    user = User(password='admin', email='admin@example.com', is_admin=False)
+    user = User(password="admin", email="admin@example.com", is_admin=False)
     user.roles.append(role)
     db.session.add(user)
 
     db.session.commit()
 
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
 
 
 if __name__ == "__main__":
