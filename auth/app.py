b64ce62d9e44894b2fcf568a90f15e0e3af616a5
vladislav erofeev <vladislav.erofeev@zyfra.com>
2023-03-27 19:09:57 +0300

    Добавил utils.py для криптографии

--- auth/app.py
+++ auth/app.py
@@ -1,11 +1,18 @@
+from flask_jwt_extended import JWTManager
+
 from db import db, init_db
 from flask import Flask
 from settings import auth_postgres_url
 
 from models import Role, User
+from utils import hash_password, is_correct_password, create_tokens
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = auth_postgres_url
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "fd08b18e863a9635534b297cc39efe99a6f49cfae6a1f33eb21f9c3dc97c0466"
+jwt = JWTManager(app)
 
 app.app_context().push()
 init_db(app)
@@ -17,14 +24,17 @@ def hello_world():
 
 
 def main():
-    role = Role(name="actor")
-    db.session.add(role)
-
-    user = User(password="admin", email="admin@example.com", is_admin=False)
-    user.roles.append(role)
-    db.session.add(user)
-
-    db.session.commit()
+    # role = Role(name="actor")
+    # db.session.add(role)
+    #
+    # user = User(password="admin", email="admin@example.com", is_admin=False)
+    # user.roles.append(role)
+    # db.session.add(user)
+    #
+    # db.session.commit()
+    print(hash_password('123456'))
+    print(is_correct_password(hash_password('123456'), '123476'))
+    print(create_tokens('test'))
 
     app.run(host="0.0.0.0", port=5000)
 
---
+++ auth/utils.py
@@ -0,0 +1,31 @@
+import hashlib
+import os
+
+from flask_jwt_extended import (create_access_token,
+                                create_refresh_token)
+
+
+def hash_password(password: str) -> str:
+    salt = os.urandom(32)
+    pw_hash = hashlib.pbkdf2_hmac("sha256", password.encode("utf-8"), salt, 100000)
+    return f"{salt.hex()}&{pw_hash.hex()}"
+
+
+def is_correct_password(correct_password: str, entered_password: str) -> bool:
+    salt, correct_pw_hash = correct_password.split("&")
+    entered_pw_hash = hashlib.pbkdf2_hmac("sha256", entered_password.encode("utf-8"), bytes.fromhex(salt), 100000)
+    if correct_pw_hash == str(entered_pw_hash.hex()):
+        return True
+    return False
+
+
+def create_tokens(identity: str) -> tuple[str, str]:
+    access_token = create_access_token(identity=identity)
+    refresh_token = create_refresh_token(identity=identity)
+    return access_token, refresh_token
+
+
+def is_correct_token():
+    pass
+
+
