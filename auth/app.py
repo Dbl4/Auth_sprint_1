2ee9f84381d394f01740335b09abad46a71b8848
vladislav erofeev <vladislav.erofeev@zyfra.com>
2023-03-30 22:04:49 +0300

    Добавил signup

--- auth/app.py
+++ auth/app.py
@@ -1,10 +1,13 @@
 from flask_jwt_extended import JWTManager
 
+from api.v1.users import auth
 from db import init_db
 from flask import Flask
+
 from settings import auth_postgres_url, settings
 
 app = Flask(__name__)
+app.register_blueprint(auth)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = auth_postgres_url
 app.config["JWT_SECRET_KEY"] = settings.jwt_secret_key
