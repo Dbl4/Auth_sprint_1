83e36681f4c010dbf7751916b0d9ca4fb3f633e3
rtmvnv <rtmvnv@yandex.ru>
2023-04-02 12:21:39 +0300

    Make functional tests directory

---
+++ auth/tests/functional/test_roles.py
@@ -0,0 +1,9 @@
+def test_home_page_post_with_fixture(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is is posted to (POST)
+    THEN check that a '405' status code is returned
+    """
+    response = test_client.post('/')
+    assert response.status_code == 405
+    assert b"Flask User Management Example!" not in response.data
