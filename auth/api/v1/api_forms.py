6835f1ff8f0c5269d5482384635daf16cf57ff2f
rtmvnv <rtmvnv@yandex.ru>
2023-04-06 13:41:36 +0300

    Add Roles endpoints

--- auth/api/v1/api_forms.py
+++ auth/api/v1/api_forms.py
@@ -1,10 +1,15 @@
 from spectree import SpecTree
 from pydantic import BaseModel
+from uuid import UUID
 
 
 class SignupForm(BaseModel):
     email: str
     password: str
+
+
+class RolesPost(BaseModel):
+    name: str
 
 
 spec = SpecTree("flask")
--- auth/api/v1/roles.py
+++ auth/api/v1/roles.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, jsonify, abort, request
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from flask import Blueprint, jsonify, request
+from sqlalchemy.exc import IntegrityError
+from api.v1.api_forms import spec, RolesPost
 
 from db import db
-from models import User, Role
-from utils import hash_password, create_tokens
+from models import Role
 
 roles = Blueprint("roles", __name__, url_prefix="/roles")
 
@@ -16,23 +16,37 @@ def get():
     return jsonify(roles)
 
 
+@spec.validate(json=RolesPost)
 @roles.post("/")
 def post():
-    name = request.json.get("name")
-    role = Role(name=name)
+    role = Role(name=request.json.get("name"))
     db.session.add(role)
     try:
         db.session.commit()
     except IntegrityError:
-        abort(409, description="Role already exists")
-    return jsonify(name=name)
+        return "Роль уже существует", 409
+    return jsonify(role.to_json())
 
 
+@spec.validate(json=RolesPost)
 @roles.put("/<uuid:role_id>/")
 def put(role_id):
-    return jsonify("put")
+    role = db.session.get(Role, role_id)
+    if not role:
+        return "Роль не найдена", 404
+    role.name = request.json.get("name")
+    try:
+        db.session.commit()
+    except IntegrityError:
+        return "Роль уже существует", 409
+    return "Роль переименована", 204
 
 
 @roles.delete("/<uuid:role_id>/")
 def delete(role_id):
-    return jsonify("delete")
+    role = db.session.get(Role, role_id)
+    if not role:
+        return "Роль не найдена", 404
+    db.session.delete(role)
+    db.session.commit()
+    return "Роль удалена", 204
