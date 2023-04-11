d5540950349167f1f23f3f6196742747c0200997
vladislav erofeev <vladislav.erofeev@zyfra.com>
2023-04-11 17:55:56 +0300

    added test_sessions

--- auth/tests/functional/test_sessions.py
+++ auth/tests/functional/test_sessions.py
@@ -1,30 +1,53 @@
 from http import HTTPStatus
+from uuid import uuid4
+
+import pytest
 
 from settings import settings
 from tests.settings import login_user, create_user
 
 
-def test_login(test_client, session, faker):
+@pytest.mark.parametrize(
+    "email, password, expected_code",
+    [
+        (settings.test_user_email, settings.test_user_password, HTTPStatus.OK),
+        ("incorrect@gmail.com", settings.test_user_password, HTTPStatus.FORBIDDEN),
+        (settings.test_user_email, "incorrect-password", HTTPStatus.FORBIDDEN),
+        (
+            "not-valid-email",
+            settings.test_user_password,
+            HTTPStatus.UNPROCESSABLE_ENTITY,
+        ),
+    ],
+)
+def test_login(
+    test_client,
+    session,
+    faker,
+    email,
+    password,
+    expected_code,
+):
     """
-    GIVEN 
-    WHEN 
-    THEN 
+    GIVEN
+    WHEN
+    THEN
     """
     create_user(session=session, admin=True)
 
     response = test_client.post(
         "/v1/sessions/",
         json={
-            "email": settings.test_user_email,
-            "password": settings.test_user_password,
+            "email": email,
+            "password": password,
             "user-agent": faker.user_agent(),
             "user-ip": faker.ipv4(),
         },
     )
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == expected_code
 
 
-def test_check(test_client, session, faker):
+def test_check(test_client, session):
     """
     GIVEN admin user is logged in
     WHEN GET /sessions/ request with access-token comes
@@ -46,5 +69,60 @@ def test_unauthorized_permissions(test_client, session):
       except POST /sessions/
     THEN HTTP code 401 is returned
     """
+    create_user(session=session, admin=True)
+    access_token, refresh_token = login_user(test_client)
+
     response = test_client.get("/v1/sessions/")
     assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+    response = test_client.delete("/v1/sessions/")
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+    response = test_client.put(
+        "/v1/sessions/",
+        headers={"Authorization": "Bearer {}".format(access_token)},
+        json={"refresh_token": uuid4()},
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+def test_logout(test_client, session):
+    create_user(session=session, admin=True)
+    access_token, refresh_token = login_user(test_client)
+    response = test_client.delete(
+        "/v1/sessions/",
+        headers={"Authorization": "Bearer {}".format(access_token)},
+    )
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_logout_all(test_client, session):
+    create_user(session=session, admin=True)
+    access_token, refresh_token = login_user(test_client)
+    response = test_client.delete(
+        "/v1/sessions/all/",
+        headers={"Authorization": "Bearer {}".format(access_token)},
+    )
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_refresh(test_client, session):
+    create_user(session=session, admin=True)
+    access_token, refresh_token = login_user(test_client)
+    response = test_client.put(
+        "/v1/sessions/",
+        headers={"Authorization": "Bearer {}".format(access_token)},
+        json={"refresh_token": refresh_token},
+    )
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_refresh_not_equal(test_client, session):
+    create_user(session=session, admin=True)
+    access_token, refresh_token = login_user(test_client)
+    response = test_client.put(
+        "/v1/sessions/",
+        headers={"Authorization": "Bearer {}".format(access_token)},
+        json={"refresh_token": refresh_token},
+    )
+    assert response.json != {"access": access_token, "refresh": refresh_token}
