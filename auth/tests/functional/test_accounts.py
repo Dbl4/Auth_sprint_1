067c420933e8179ee10c77d8c4c7a3b2b2604ed8
Сергей К <medium.well@yandex.ru>
2023-04-12 15:58:23 +0300

    Реализованы тесты для `DELETE /accounts` и неавторизованного доступа

--- auth/tests/functional/test_accounts.py
+++ auth/tests/functional/test_accounts.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+from faker import Faker
 from flask_sqlalchemy.session import Session
 from werkzeug.test import Client
 
@@ -28,6 +29,25 @@ def test_get(test_client: Client, session: Session) -> None:
     assert len(response.json["history"]) == history_len + 1
 
 
+def test_post(test_client: Client, faker: Faker) -> None:
+    email = faker.email('test', 'example123.com')
+    print(email)
+    response = test_client.post(
+        "/v1/accounts/",
+        json={'email': email, 'password': faker.password()},
+    )
+    assert response.status_code == HTTPStatus.OK
+    response = test_client.post(
+        "/v1/accounts/",
+        json={'email': email, 'password': faker.password()},
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+
+
+def test_patch(test_client: Client, session: Session, faker: Faker) -> None:
+    pass
+
+
 def test_delete(test_client: Client, session: Session) -> None:
     create_user(session=session)
     access_token, _ = login_user(test_client)
@@ -42,4 +62,12 @@ def test_delete(test_client: Client, session: Session) -> None:
         assert True
     else:
         assert False
-    
+
+
+def test_unauthorized_permissions(test_client: Client):
+    response = test_client.get("/v1/accounts/")
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    response = test_client.patch("/v1/accounts/")
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    response = test_client.delete("/v1/accounts/")
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
